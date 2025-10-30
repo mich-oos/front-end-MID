6b383561c4914f92c96494a1d484c0a9eba895c9
Astrid Mongi <12410384@student.unklab.ac.id>
2025-10-30 15:20:16 +0800

    inisialisasi generation

---
+++ src/components/footer/footer.js
@@ -0,0 +1,9 @@
+const Footer = ({ title }) => {
+  return (
+    <div className="bg-gray-800 text-white p-4 rounded-lg shadow mt-4">
+      <p>{title}</p>
+    </div>
+  );
+};
+
+export default Footer;
